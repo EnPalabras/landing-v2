a9ec72f6d4e2f3c23d6a6a60af699654f446f42f
Agustin Iglesias <agusiglesias72@gmail.com>
2024-09-16 13:13:03 -0300

    add sitemap and robots

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { GoogleTagManager } from '@next/third-parties/google'
-
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header/Header'
