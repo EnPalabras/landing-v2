24fa5fa81e5ab659322e246b9adc9ecf4477bf1a
unknown <agusiglesias72@gmail.com>
2025-03-27 16:00:46 -0300

    add

--- src/components/NewHero.tsx
+++ src/components/NewHero.tsx
@@ -65,7 +65,7 @@ export default function HeroSection() {
   };
 
   return (
-    <section className="w-full relative overflow-hidden max-w-7xl">
+    <section className="w-full relative overflow-hidden max-w-7xl pb-10">
 
       <motion.div
         className="container py-16 lg:py-20 relative z-10"
